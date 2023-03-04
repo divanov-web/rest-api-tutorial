0eff3b76945407b33b89b2076c72bf7a3d13e7c4
divanov <cool-di@mail.ru>
2023-03-04 17:45:00 +0500

    Lesson 3 and init

---
+++ internal/user/handler.go
@@ -0,0 +1,62 @@
+package user
+
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+	"rest-api-tutorial/internal/handlers"
+	"rest-api-tutorial/pkg/logging"
+)
+
+const (
+	usersURL = "/users"
+	userURL  = "/users/:uuid"
+)
+
+type handler struct {
+	logger *logging.Logger
+}
+
+func NewHandler(logger *logging.Logger) handlers.Handler {
+	return &handler{
+		logger: logger,
+	}
+}
+
+func (h *handler) Register(router *httprouter.Router) {
+	router.GET(usersURL, h.GetList)
+	router.POST(usersURL, h.CreateUser)
+	router.GET(userURL, h.GetUserByUUID)
+	router.PUT(userURL, h.UpdateUser)
+	router.PATCH(userURL, h.PartiallyUpdateUser)
+	router.DELETE(userURL, h.DeleteUser)
+}
+
+func (h *handler) GetList(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("List of users"))
+}
+
+func (h *handler) CreateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(201)
+	w.Write([]byte("create user"))
+}
+
+func (h *handler) GetUserByUUID(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(200)
+	w.Write([]byte("get user by id"))
+}
+
+func (h *handler) UpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("full update user"))
+}
+
+func (h *handler) PartiallyUpdateUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("partially update user"))
+}
+
+func (h *handler) DeleteUser(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	w.WriteHeader(204)
+	w.Write([]byte("delete user"))
+}
